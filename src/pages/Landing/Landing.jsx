4f6f88a18bc0bf9e6b2b5dde7b142ef76fb56d9d
Joe Malatesta <joemalatesta@msn.com>
2023-10-17 11:14:53 -0400

    add main background

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,7 @@ const Landing = ({ user }) => {
 
 
   return (
-    <main>
+    <main className="landing-bg">
       <h1>Tavern's Tourney's</h1>
       <h3>hello, {user ? user.name : 'friend'}</h3>
     </main>
