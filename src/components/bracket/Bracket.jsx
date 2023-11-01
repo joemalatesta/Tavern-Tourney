51e07967af8187d21808f05de95f4f103c7f63c7
Joe Malatesta <joemalatesta@msn.com>
2023-11-01 11:32:39 -0400

    have arrays for all maches. need to work on sorting and checkboxes next

--- src/components/bracket/Bracket.jsx
+++ src/components/bracket/Bracket.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import SingleMatch from './SingleMatch'
 
 const Bracket = ({ playerObj }) => {
-  const [round, setRound] = useState([[null, null], [null, null]]);
-
+  const [round, setRound] = useState([[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null]])
+      
+  
   const handleRoundPlayers = (player) => {
     const playerIdx = getIndex(player);
 
@@ -13,18 +14,63 @@ const Bracket = ({ playerObj }) => {
         const nullIndex = updatedRound[0].indexOf(null);
         updatedRound[0][nullIndex] = player;
         setRound(updatedRound);
-      } else {
-        if (round[1].includes(null)) {
-          const updatedRound = [...round];
-          const nullIndex = updatedRound[1].indexOf(null);
-          updatedRound[1][nullIndex] = player;
-          setRound(updatedRound);
-        } else {
-          console.log("Both rounds are complete, cannot add more players.");
-        }
-      }
-    } else {
-      console.log("Player index is out of range.");
+      } 
+    } 
+    if (playerIdx === 4 || playerIdx === 5 || playerIdx === 6 || playerIdx === 7) {
+      if (round[1].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[1].indexOf(null);
+        updatedRound[1][nullIndex] = player;
+        setRound(updatedRound);
+      } 
+    }
+    if (playerIdx === 8 || playerIdx === 9 || playerIdx === 10 || playerIdx === 11) {
+      if (round[2].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[2].indexOf(null);
+        updatedRound[2][nullIndex] = player;
+        setRound(updatedRound);
+      } 
+    } 
+    if (playerIdx === 12 || playerIdx === 13 || playerIdx === 14 || playerIdx === 15) {
+      if (round[3].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[3].indexOf(null);
+        updatedRound[3][nullIndex] = player;
+        setRound(updatedRound);
+      } 
+    }
+    if (playerIdx === 16 || playerIdx === 17 || playerIdx === 18 || playerIdx === 19) {
+      if (round[4].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[4].indexOf(null);
+        updatedRound[4][nullIndex] = player;
+        setRound(updatedRound);
+      } 
+    } 
+    if (playerIdx === 20 || playerIdx === 21 || playerIdx === 22 || playerIdx === 23) {
+      if (round[5].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[5].indexOf(null);
+        updatedRound[5][nullIndex] = player;
+        setRound(updatedRound);
+      } 
+    }
+    if (playerIdx === 24 || playerIdx === 25 || playerIdx === 26 || playerIdx === 27) {
+      if (round[6].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[6].indexOf(null);
+        updatedRound[6][nullIndex] = player;
+        setRound(updatedRound);
+      } 
+    } 
+    if (playerIdx === 28 || playerIdx === 29 || playerIdx === 30 || playerIdx === 31) {
+      if (round[7].includes(null)) {
+        const updatedRound = [...round];
+        const nullIndex = updatedRound[7].indexOf(null);
+        updatedRound[7][nullIndex] = player;
+        setRound(updatedRound);
+      } 
     }
     console.log(round);
   };
@@ -44,10 +90,10 @@ const Bracket = ({ playerObj }) => {
         />
       ))}
       {/* Display rounds */}
-      {round.map((match, index) => (
+      {round?.map((match, index) => (
         <div key={index}>
           {match.map((player, idx) => (
-            <div key={idx}>{player ? player.name : "Waiting..."}</div>
+            <div key={idx}>{ player ? player.name : null }</div>
           ))}
         </div>
       ))}
--- src/components/bracket/SingleMatch.jsx
+++ src/components/bracket/SingleMatch.jsx
@@ -1,7 +1,7 @@
 
 const SingleMatch = (props) => {
-
-  // here is where I should pair the players into a match
+  // need to have a way to identify which round I'm playing in. then save the players into the array of arrays for that round. might need to adjust the model on the back end to have different rounds. this way the bracket can handle people playing faster than the current round.
+  // here is where I should pair the players into a matchbring in playerObj and 
   // then send the winner to the bracket level
   // Should have winner here.
   // need to have the winner function passed to here
